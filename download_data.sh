be795e37b3bedb4fcae6f9ecef3c1dc60e970849
Yuki Tsuji <yukitsuji020832@gmail.com>
2017-05-24 11:22:16 +0900

    Add sh file for build this project

---
+++ download_data.sh
@@ -0,0 +1,5 @@
+cd MNIST_data
+curl -O http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
+curl -O http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
+curl -O http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
+curl -O http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
